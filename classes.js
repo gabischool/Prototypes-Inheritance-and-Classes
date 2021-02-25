d4a58463093b6847a05f4f59981b385f1d472429
zakabdi <zakariaabdi104@gmail.com>
2021-02-24 19:01:54 -0500

    i did it late

--- classes.js
+++ classes.js
@@ -8,3 +8,57 @@
 
 
 // Extend Person class and create one class for each member of your family.
+
+
+class Person {
+    constructor(familyMember){
+        this.name = familyMember.name;
+        this.Age = familyMember.Age;
+        this.Hobby = familyMember.Hobby;
+    }
+    
+    Speak (){
+        return `Hello, my name is ${this.name} and I am ${this.Age } years old. I love ${this.Hobby}`
+    }
+}
+
+class Child extends Person{
+    constructor(children){
+        super(children)
+
+    }
+}
+
+
+const dad = new Person ({
+    name:'abdi',
+    Age:46, 
+    Hobby:"Reading Qurran"
+});
+
+const FirstSon = new Child ({
+    name:'Zakaria',
+    Age:23, 
+    Hobby:"Reading Qurran"
+});
+
+const secondSon = new Child ({
+    name:'abdirashid',
+    Age:22, 
+    Hobby:"Reading Qurran"
+});
+const firstDaughter = new Child ({
+    name:'ayaan',
+    Age:12, 
+    Hobby:"Reading Qurran"
+});
+
+
+
+
+
+
+console.log(dad.Speak());
+console.log(FirstSon.Speak());
+console.log(secondSon.Speak());
+console.log(firstDaughter.Speak());
